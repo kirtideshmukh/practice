5d07c31331fc33b5583953e01d0fead541b26c6e
Kirti Deshmukh <deshmukhkirtib@gmail.com>
2021-03-13 00:58:21 +0530

    Solve check if palindrome using recursion

--- strings/CheckIfPalindrome.java
+++ strings/CheckIfPalindrome.java
@@ -4,15 +4,20 @@
  * Complexity  :   O(n) Time | O(1) Space ∈ n is the length of the input string
  */
 
+import java.util.*;
+
 class Program {
-    public static boolean isPalindrome(String inputString) {
-        Integer leftCounter =0;
-        Integer rightCounter = inputString.length() - 1;
-        for(;leftCounter< rightCounter; leftCounter++, rightCounter--) {
-            if( inputString.charAt(leftCounter) != inputString.charAt(rightCounter)) {
-                return false;
-            }
-        }
-        return true;
+    public static boolean isPalindrome(String str, int leftCounter, int rightCounter) {
+
+        return leftCounter >= rightCounter
+                ? true
+                : (str.charAt(leftCounter) == str.charAt(rightCounter)
+                && isPalindrome(str, ++leftCounter, --rightCounter));
+    }
+
+    public static boolean isPalindrome(String str) {
+        Integer leftCounter = 0;
+        Integer rightCounter = str.length() - 1;
+        return isPalindrome(str, leftCounter, rightCounter);
     }
 }
