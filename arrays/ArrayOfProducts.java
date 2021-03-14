b10aa5f1d5eb9029528f1ce0cbeb1516e9a825dc
Kirti Deshmukh <deshmukhkirtib@gmail.com>
2021-03-14 20:51:09 +0530

    Refactor array of products | Improve time complexity

--- arrays/ArrayOfProducts.java
+++ arrays/ArrayOfProducts.java
@@ -2,25 +2,38 @@
  * Domain       :   Array
  * Level        :   Medium
  * Problem      :   output[i] = the product of every number in the input array other than input[i]
- * Complexity   :   O(n^2) Time | O(n) Space ∈ n is the length input array
+ * Complexity   :   O(n) Time | O(n) Space ∈ n is the length input array
  * Sample input :
-    array       =   [5, 1, 4, 2]
-    output      =   [8, 40, 10, 20]
+ array       =   [5, 1, 4, 2]
+ output      =   [8, 40, 10, 20]
+
+ * Approach     :   Calculate and store left and right products for ith element.
+                    output[i] = leftProducts[i] * rightProducts[i]
  */
+
 import java.util.*;
 
 class Program {
     public int[] arrayOfProducts(int[] array) {
         int size = array.length;
         int[] result = new int[size];
+        int[] leftProducts = new int[size];
+        Arrays.fill(leftProducts, 1);
+        int[] rightProducts = new int[size];
+        Arrays.fill(rightProducts, 1);
+        int leftProduct = 1;
+        for (int i = 1; i < size; i++) {
+            leftProduct *= array[i - 1];
+            leftProducts[i] = leftProduct;
+        }
+        int rightProduct = 1;
+        for (int i = size - 2; i >= 0; i--) {
+            rightProduct *= array[i + 1];
+            rightProducts[i] = rightProduct;
+        }
+
         for (int i = 0; i < size; i++) {
-            int productOfRestOfElements = 1;
-            for (int j = 0; j < size; j++) {
-                if (i != j) {
-                    productOfRestOfElements *= array[j];
-                }
-            }
-            result[i] = productOfRestOfElements;
+            result[i] = leftProducts[i] * rightProducts[i];
         }
         return result;
     }
