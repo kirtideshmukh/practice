ab16e9f415dc86bd8859a0aa3d7852918bd38a3a
Kirti Deshmukh <deshmukhkirtib@gmail.com>
2021-03-16 23:40:19 +0530

    Refactor find first duplicate | Improve time complexity

--- arrays/FindFirstDuplicateValue.java
+++ arrays/FindFirstDuplicateValue.java
@@ -4,7 +4,7 @@
  * Problem      :   Given an array of integers between 1 and n inclusive, where n is the length of the array,
                     write a function that returns the first integer that appears more than once
                     (when the array is read from left to right).
- * Complexity   :   O(n^2) Time | O(1) Space ∈ n is the size of input array
+ * Complexity   :   O(n) Time | O(c) Space ∈ n is the size of input array, c is the unique numbers in the array
  * Sample input :
     array  = [1, 2, 3, 3, 2]
     output = 3
@@ -15,18 +15,13 @@ class Program {
 
     public int firstDuplicateValue(int[] array) {
         int firstDuplicateValue = -1;
-        int minimumSecondIndex = array.length;
-        for (int i = 0; i < array.length - 1; i++) {
+        Set<Integer> visitedNumbers = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
             int currentCellValue = array[i];
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j]) {
-                    if (j < minimumSecondIndex) {
-                        firstDuplicateValue = currentCellValue;
-                        minimumSecondIndex = j;
-                        break;
-                    }
-                }
+            if (visitedNumbers.contains(currentCellValue)) {
+                return currentCellValue;
             }
+            visitedNumbers.add(currentCellValue);
         }
         return firstDuplicateValue;
     }
